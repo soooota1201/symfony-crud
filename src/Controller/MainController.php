c7d6f1ebcb9ab31712275d806cc90bda071f6149
Sota Nakai <soooota1201@gmail.com>
2021-10-31 23:17:30 +0900

    add crud

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -16,17 +16,64 @@ class MainController extends AbstractController
      */
     public function index(): Response
     {
+        $data = $this->getDoctrine()->getRepository(Crud::class)->findAll();
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'list' => $data
         ]);
     }
 
   /**
    * @Route("create", name="create")
    */
-  public function create(Reauest $reauest)
+  public function create(Request $request)
   {
-     $crud = new Crud();
-     $form = new Crud();
+      $crud = new Crud();
+      $form = $this->createForm(CrudType::class, $crud);
+      $form->handleRequest($request);
+      if($form->isSubmitted() && $form->isValid()) {
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($crud);
+        $em->flush();
+        $this->addFlash('notice', 'Submitted Successfully');
+        return $this->redirectToRoute('main');
+      }
+
+      return $this->render('main/create.html.twig', [
+        'form' => $form->createView()
+      ]);
+  }
+
+  /**
+   * @Route("/update/{id}", name="update")
+   */
+  public function update(Request $request, $id)
+  {
+    $crud = $this->getDoctrine()->getRepository(Crud::class)->find($id);
+    $form = $this->createForm(CrudType::class, $crud);
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+      $em = $this->getDoctrine()->getManager();
+      $em->persist($crud);
+      $em->flush();
+      $this->addFlash('notice', 'Update Successfully');
+      return $this->redirectToRoute('main');
+    }
+
+    return $this->render('main/update.html.twig', [
+      'form' => $form->createView()
+    ]);
+  }
+
+  /**
+   * @Route("/delete/{id}", name="delete")
+   */
+  public function delete($id)
+  {
+    $data = $this->getDoctrine()->getRepository(Crud::class)->find($id);
+    $em = $this->getDoctrine()->getManager();
+    $em->remove($data);
+    $em->flush();
+    $this->addFlash('delete', 'Deleted Successfully');
+    return $this->redirectToRoute('main');
   }
 }
